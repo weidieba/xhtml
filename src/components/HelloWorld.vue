dc433e9e59c52b9969583e63c8d3acf54da3f9de
luowei <18337363971@163.com>
2020-07-11 14:50:58 +0800

    feature(sassloderinstall): 添加sass语言配置wepacksass的loders

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -85,19 +85,21 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+    name: 'HelloWorld',
+    data () {
+        return {
+            msg: 'Welcome to Your Vue.js App'
+        }
     }
-  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss" scoped>
+$textcolor:red;
 h1, h2 {
   font-weight: normal;
+  color: $textcolor;
 }
 ul {
   list-style-type: none;
