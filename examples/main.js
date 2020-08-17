fb1cab827912a04897ee49590efc9550b79cc100
v_weiiluo <v_weiiluo@tencent.com>
2020-08-17 15:22:28 +0800

    feature(addComponents): 添加打包组件以及测试组件

--- examples/main.js
+++ examples/main.js
@@ -4,8 +4,10 @@ import Vue from 'vue'
 import App from './App'
 import VueRouter from 'vue-router'
 import ConfigRouter from './router.config'
+import Xhtml from '../packages'
 Vue.config.productionTip = false
 Vue.use(VueRouter)
+Vue.use(Xhtml)
 const router = new VueRouter(ConfigRouter)
 /* eslint-disable no-new */
 new Vue({
--- examples/router.config.js
+++ examples/router.config.js
@@ -2,6 +2,6 @@ let routers = []
 routers.push({
     path: '/test',
     name: 'test',
-    component: r => require.ensure([], () => r(require('./docs/test.md')))
+    component: r => require.ensure([], () => r(require('./components/HelloWorld.vue')))
 })
 export default routers
